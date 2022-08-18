fefcff83188e44b8fcb0a0ef3fcf8311a4e2993b
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2022-08-18 18:36:30 +0600

    create office

--- client/src/components/Main/Office/Office.jsx
+++ client/src/components/Main/Office/Office.jsx
@@ -1,8 +1,29 @@
+import s from './Office.module.scss'
+import office from '../../../assets/images/office.jpg'
+
 const Office = () => {
     return (
-        <div>
-            Office
+      <div className={s.office}>
+        <h2>OFFICE</h2>
+
+        <div className={s.content}>
+          <div>
+            <img src={office} alt="office" />
+          </div>
+
+          <div className={s.info}>
+                <h3>Gaudi Studio</h3>
+                <p>Lorem, ipsum dolor.</p>
+                <p>1004 lorem</p>
+                <p>lorem/ipsum</p>
+
+                <div className={s.contact}>
+                    <span>+41 21 624 44 44</span>
+                    <span>test.site@gmail.com</span>
+                </div>
+          </div>
         </div>
+      </div>
     );
 }
 
