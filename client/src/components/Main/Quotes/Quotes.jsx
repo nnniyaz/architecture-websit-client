2fa8983129f330e8d12c8abccf8ee039345c90df
Niyaz Nassyrov <nassyrovich@gmail.com>
2022-08-19 22:02:51 +0600

    responsive update

--- client/src/components/Main/Quotes/Quotes.jsx
+++ client/src/components/Main/Quotes/Quotes.jsx
@@ -8,12 +8,15 @@ const Quotes = () => {
                 <div className={classes.grandient}>
                     <div className={classes.title}>PEOPLE</div>
                     <div className={classes.topic}>
-                        <div className={classes.topictext}>We are dedicated to providing our clients with the best that design has to offer.</div>
+                        <div className={classes.topictext}>We are dedicated to</div>
+                        <div className={classes.topictext}>providing our clients</div>
+                        <div className={classes.topictext}>with the best that</div>
+                        <div className={classes.topictext}>design has to offer.</div>
                         <div className={classes.author}>Frank Gehry</div>
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
