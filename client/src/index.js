bda2c43aaa5a1f4f7bc6ebda94e744d47a13ab2b
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2022-08-19 18:15:28 +0600

    hello

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Main from './components/Main/Main';
+
+import Layout from './components/pages/Admin/Layout/Layout';
+import Admin from './components/pages/Admin/Mainpage/Admin';
+import Users from './components/pages/Admin/Users/Users';
+import Login from './components/pages/Login/Login';
+import Registration from './components/pages/Registration/Registration';
+import Reset from './components/pages/Restore/Reset';
+import Restore from './components/pages/Restore/Restore';
+import PersistLogin from './components/PersistLogin';
+import RequireAuth from './components/RequireAuth';
+import { AuthProvider } from './context/auth-provider';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
@@ -10,12 +21,27 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Main />}></Route>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route element={<App />}>
+            <Route path='/' element={<Main />}></Route>
+          </Route>
+
+          <Route path='admin/login' element={<Login />} />
+          <Route path='admin/registration' element={<Registration />} />
+          <Route path='admin/restore' element={<Restore />} />
+          <Route path='admin/reset/:email/:id/:link' element={<Reset />} />
+
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth />}>
+              <Route element={<Layout />}>
+                <Route path='/admin' element={<Admin />} />
+                <Route path='/admin/users' element={<Users />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </Provider>
 );
-
